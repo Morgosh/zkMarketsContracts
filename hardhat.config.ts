0d74285f27416e86f5a10a74f1db89baaee18594
morgosh <ninoserec@gmail.com>
2025-09-25 14:58:41 +0200

    up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,8 +81,8 @@ const config: HardhatUserConfig = {
         network: "abstract-testnet",
         chainId: 11124,
         urls: {
-          //apiURL: "https://api.etherscan.io/v2/api?chainid=11124",
-          apiURL: "https://api-sepolia.abscan.org/api",
+          apiURL: "https://api.etherscan.io/v2/api?chainid=11124",
+          //apiURL: "https://api-sepolia.abscan.org/api",
           browserURL: "https://sepolia.abscan.org/",
         },
       },
@@ -90,8 +90,8 @@ const config: HardhatUserConfig = {
         network: "abstract",
         chainId: 2741,
         urls: {
-          //apiURL: "https://api.etherscan.io/v2/api?chainid=2741",
-          apiURL: "https://api.abscan.org/api",
+          apiURL: "https://api.etherscan.io/v2/api?chainid=2741",
+          //apiURL: "https://api.abscan.org/api",
           browserURL: "https://abscan.org/",
         },
       },
